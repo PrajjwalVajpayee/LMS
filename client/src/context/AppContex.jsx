547cf5256f8cd8d062a46bf65013b86b0802c402
PrajjwalVajpayee <122729018+PrajjwalVajpayee@users.noreply.github.com>
2025-02-28 01:26:25 +0530

    create calculateRating function

--- client/src/context/AppContex.jsx
+++ client/src/context/AppContex.jsx
@@ -8,6 +8,7 @@ export const AppContextProvider = (props) =>{
 
     const currency = import.meta.env.VITE_CURRENCY;
     const [allCourses,setAllCourses] = useState([]);
+    const [isEducator,setIsEducator] = useState(true);
     const navigate = useNavigate()
 
     // Fetch all courses
@@ -15,12 +16,25 @@ export const AppContextProvider = (props) =>{
     const fetchAllCourses = async ()=>{
    setAllCourses(dummyCourses);
     }
+    //   function to calc avg rating
 
-  useEffect(()=>{
+    const calculateRating = (course)=>{
+       if(course.courseRatings.length === 0){
+        return 0;
+       }
+       let totalRating = 0;
+       course.courseRatings.forEach(rating =>{
+        totalRating+=rating.rating;
+       })
+       return totalRating/course.courseRatings.length;
+}  
+
+useEffect(()=>{
   fetchAllCourses();
   },[])  
  const value ={
-    currency,allCourses,navigate
+    currency,allCourses,navigate,
+    calculateRating,isEducator,setIsEducator
  }
 
 
