18e49fd5828fa3e17b7003e7e0088907ff1ce87e
PrajjwalVajpayee <122729018+PrajjwalVajpayee@users.noreply.github.com>
2025-03-01 02:41:51 +0530

    use humanize duration package for calculating time  create diffrent functiom like calculateChapterTime,calculateCourseDuration,calculateNoOfLectures

--- client/src/context/AppContex.jsx
+++ client/src/context/AppContex.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import humanizeDuration from "humanize-duration";
 
 export const AppContext = createContext();
 
@@ -28,13 +29,40 @@ export const AppContextProvider = (props) =>{
        })
        return totalRating/course.courseRatings.length;
 }  
+//    function to calculate chapter Time
+const calculateChapterTime = (chapter)=>{
+   let time =0;
+   chapter.chapterContent.map((lecture)=>time += lecture.lectureDuration)
+   return humanizeDuration(time*60*1000 ,{unit:['h','m']});
+
+}
+//  fuction to calculate course Duration
+
+const calculateCourseDuration = (course) =>{
+    let time=0;
+    course.courseContent.map((chapter)=>chapter.chapterContent.map((lecture)=>time += lecture.lectureDuration))
+    return humanizeDuration(time*60*1000 ,{unit:['h','m']});
+}
+
+//  function to claculate no of lectures in the course
+
+ const calculateNoOfLectures = (course)=>{
+    let totalLectures =0;
+    course.courseContent.forEach(chapter => {
+        if(Array.isArray(chapter.chapterContent)){
+            totalLectures += chapter.chapterContent.length;
+        }
+    })
+    return totalLectures;
+ }
 
 useEffect(()=>{
   fetchAllCourses();
   },[])  
  const value ={
     currency,allCourses,navigate,
-    calculateRating,isEducator,setIsEducator
+    calculateRating,isEducator,setIsEducator,
+    calculateChapterTime,calculateNoOfLectures ,calculateCourseDuration
  }
 
 
