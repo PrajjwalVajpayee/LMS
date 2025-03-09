46a89a28d55a9423daa5bdcd5c6b4e039412fa9c
PrajjwalVajpayee <122729018+PrajjwalVajpayee@users.noreply.github.com>
2025-03-09 23:49:31 +0530

    add authorization to educator section

--- client/src/context/AppContex.jsx
+++ client/src/context/AppContex.jsx
@@ -2,7 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import humanizeDuration from "humanize-duration";
-
+import {useAuth, useUser} from "@clerk/clerk-react"
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) =>{
@@ -12,6 +12,8 @@ export const AppContextProvider = (props) =>{
     const [isEducator,setIsEducator] = useState(true);
     const [enrolledCourses,setEnrolledCourses] = useState([]);
     const navigate = useNavigate()
+    const {getToken} = useAuth()
+    const {user} = useUser();
 
     // Fetch all courses
 
@@ -68,10 +70,22 @@ const calculateCourseDuration = (course) => {
 const fetchEnrolledCourses =async()=>{
     setEnrolledCourses(dummyCourses);
 }
+
+const logToken = async()=>{
+    console.log(await getToken());
+    
+}
 useEffect(()=>{
   fetchAllCourses();
   fetchEnrolledCourses();
-  },[])  
+  },[]) 
+  
+  useEffect(()=>{
+   if(user){
+      logToken()
+   }
+  },[user])
+
  const value ={
     currency,allCourses,navigate,
     calculateRating,isEducator,setIsEducator,
