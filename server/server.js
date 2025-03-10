de2f4ab77788a571318703f6bfe7907add14c8bc
PrajjwalVajpayee <122729018+PrajjwalVajpayee@users.noreply.github.com>
2025-03-10 23:02:15 +0530

    add all the routes

--- server/server.js
+++ server/server.js
@@ -2,13 +2,23 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectdb from './configs/mongodb.js';
-import { clerkWebhooks } from './controllers/webhooks.js';
+import { clerkWebhooks, stripeWebHooks } from './controllers/webhooks.js';
+import educatorRouter from './routes/educatorRoutes.js';
+import { clerkMiddleware } from '@clerk/express';
+import connectCloudinary from './configs/cloudinary.js';
+import courseRouter from './routes/courseRoute.js';
+import userRouter from './routes/UserRoutes.js';
 
 const app = express();
 
 // connect to database
 await connectdb();
+await connectCloudinary();
+
+
+
 app.use(cors());
+app.use(clerkMiddleware())
 
 
 app.get('/',(req,res)=>{
@@ -16,6 +26,10 @@ app.get('/',(req,res)=>{
 })
 
 app.post('/clerk', express.json(),clerkWebhooks)
+app.use('/api/educator',express.json(),educatorRouter)
+app.use('/api/course',express.json(),courseRouter)
+app.use('/api/user',express.json(),userRouter)
+app.post('/stripe',express.raw({type:'application/json'}),stripeWebHooks)
 
 const PORT = process.env.PORT || 5000;
 
