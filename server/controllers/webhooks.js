2ed4f888f5cae352208853d6d29587dd83dc4024
PrajjwalVajpayee <122729018+PrajjwalVajpayee@users.noreply.github.com>
2025-03-11 16:01:08 +0530

    some changes

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -6,7 +6,7 @@ import Course from "../models/Course.js";
 
 export const clerkWebhooks = async (req, res) => {
   try {
-    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET_KEY);
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
  
     await whook.verify(JSON.stringify(req.body), {
       "svix-id": req.headers["svix-id"],
