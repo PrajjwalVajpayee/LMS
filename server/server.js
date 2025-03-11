30caa3e3c8c86b267950deb0af8f5c4f96a32eef
PrajjwalVajpayee <122729018+PrajjwalVajpayee@users.noreply.github.com>
2025-03-11 21:08:37 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.get('/',(req,res)=>{
     res.send("Hello World");
 })
 
-app.post('/clerk', express.json(),clerkWebhooks)
+app.post('/clerk',express.json(),clerkWebhooks)
 app.use('/api/educator',express.json(),educatorRouter)
 app.use('/api/course',express.json(),courseRouter)
 app.use('/api/user',express.json(),userRouter)
