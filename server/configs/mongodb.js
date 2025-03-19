83c30d7ee57ae4c3fdde15a199af2c14f637ca18
PrajjwalVajpayee <122729018+PrajjwalVajpayee@users.noreply.github.com>
2025-03-19 22:33:29 +0530

    changes

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -6,7 +6,7 @@ const connectdb = async()=>{
         console.log('Database Connected')
         
     })
-    await mongoose.connect(`${process.env.MONGODB_URI}`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
 }
 
 export default connectdb;
